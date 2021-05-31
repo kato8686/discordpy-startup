1025394c7c2b8f70d348ad732493a535c901db3d
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-05-31 13:06:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,9 +11,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-async def on_massage(m):
-    if m.content == 'y.hello':
-        await m.channel.send('hello')
 
 
 @bot.command()
