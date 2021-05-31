a2d58ab141a436f44b738447ead74365e42c87d2
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-05-31 21:44:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,9 @@ import random
 
 bot = commands.Bot(command_prefix='y.', help_command=None)
 token = os.environ['DISCORD_BOT_TOKEN']
+slot = []
+slotbool = False
+global slotbool
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -28,13 +31,22 @@ async def pong(ctx):
     await ctx.channel.send('ぴんぐぽーんぐ♪')
 @bot.command()
 async def slot(ctx, arg):
+    await ctx.channel.send(f'{arg}回スロットをします\n「y.stop」とそのチャンネルで発言すると止まります')
+    slotbool = True
+    slot.append([ctx.author.id, ctx.channel.id])
     for i in range(int(arg)):
-        a,b,c = random.randint(1,9),random.randint(1,9),random.randint(1,9)
-        msg = await ctx.channel.send(f'{a} {b} {c}')
-        if a == b == c:
-            await ctx.author.send(f'当たったよ\n{msg.jump_url}')
-            id = 848897434063339541
-            channel = bot.get_channel(id)
-            await channel.send(f'スロット当選\n{msg.jump_url}\nユーザー名{ctx.author.name}\nチャンネル名{ctx.channel.name}')
+        if slotbool:
+            a,b,c = random.randint(1,9),random.randint(1,9),random.randint(1,9)
+            msg = await ctx.channel.send(f'{a} {b} {c}')
+            if a == b == c:
+                await ctx.author.send(f'当たったよ\n{msg.jump_url}')
+                id = 848897434063339541
+                channel = bot.get_channel(id)
+                await channel.send(f'スロット当選\n{msg.jump_url}\nユーザー名　{ctx.author.name}\nチャンネル名 {ctx.channel.name}\nギルド名　{ctx.guild.name}')
+@bot.command()
+async def stop(ctx):
+    for i in slot:
+        if ctx.author.id in i and ctx.channel.id in i:
+            slotbool = False
 
 bot.run(token)
