7b7315f988e3205b16645fa533ad2cbf8b01b0ed
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-05-31 12:35:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,13 +11,14 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+async def on_message(ctx):
+    if ctx.content == 'hello':
+        await ctx.send('hello!!!')
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    if ctx.content == 'hello':
-        await ctx.send('hello!')
 
 
 bot.run(token)
