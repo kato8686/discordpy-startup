b793a7ef20f6c30aafae08cd15b6a54058be647f
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-05-31 13:15:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,7 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send('hello')
+async def hello(ctx, arg):
+    await ctx.send(arg)
 
 bot.run(token)
