802a3a940ae4d3379064df9a5303457accd0385b
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-05-31 13:49:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,9 @@ async def on_command_error(ctx, error):
 async def hello(ctx):
     await ctx.send('good')
 async def ping(ctx):
-    await ctx.send('pngだよ！！！！！！！！！')
+    await ctx.send('pongだよ！！！！！！！！！')
+async def help(ctx):
+    embed = discord.Embed(title='Help', description='y.ping：応答速度を測ります（手動）\ny.hello：なんか返します')
+    await ctx.send(embed=embed)
 
 bot.run(token)
