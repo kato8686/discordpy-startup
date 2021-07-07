ba01e253eabefa8056dd22d0b77b006e9cb52bac
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-07-07 19:39:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,7 @@ bot = commands.Bot(command_prefix='y.', help_command=None)
 token = os.environ['DISCORD_BOT_TOKEN']
 admin = [790054604799868939, 802152878855684106, 695996824112332887, 594404458327310336]
 owner = [802152878855684106]
+global con
 con = False
 
 @bot.event
