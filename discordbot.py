4b85563e30a62bcbc351f09053a8ae778bad76d8
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-14 17:12:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,11 @@
 import MySQLdb
 
 # 接続する
- conn = MySQLdb.connect(
- user='root',
- passwd='root',
- host='localhost',
- db='mysql')
+conn = MySQLdb.connect(
+user='root',
+passwd='root',
+host='localhost',
+db='mysql')
 
 # 接続を閉じる
- con.close
+con.close
