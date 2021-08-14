b4ad3f5f395c2a265c4f2e1614d3af440424b2e9
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-14 17:43:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,12 @@
-import MySQLdb
+import os
+import psycopg2
 
-# 接続する
-con = MySQLdb.connect(
-user='qpjacaujnkxyil',
-passwd='57acd55734d09130ac9abf498de632f05969bd0e0b799f90490728e2e40abcc4',
-host='ec2-23-23-128-222.compute-1.amazonaws.com',
-db='dbmcuoiouunpmj')
+def get_connection():
+    dsn = os.environ['DATABASE_URL']
+    return psycopg2.connect(dsn)
 
-# 接続を閉じる
-con.close
+conn = get_connection()
+cur = conn.cursor()
+cur.execute('SELECT * FROM users')
+cur.close()
+conn.close()
