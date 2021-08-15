1a49ef421ab89cc0de429c6e16c372c69c252e99
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-15 18:51:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,3 @@
 import psycopg2
-connection = psycopg2.connect("host=192.168.24.97 port=9403 dbname=sampledb user=sayamada password=pssword")
+connection = psycopg2.connect("host=ec2-23-23-128-222.compute-1.amazonaws.com port=5432 dbname=dbmcuoiouunpmj user=qpjacaujnkxyil password=57acd55734d09130ac9abf498de632f05969bd0e0b799f90490728e2e40abcc4")
 print(connection.get_backend_pid())
