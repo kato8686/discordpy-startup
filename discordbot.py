a69b62e21bea42a66af09b661da751fed1776d7e
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-15 19:40:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 import psycopg2
 connection = psycopg2.connect("host=ec2-23-23-128-222.compute-1.amazonaws.com port=5432 dbname=dbmcuoiouunpmj user=qpjacaujnkxyil password=57acd55734d09130ac9abf498de632f05969bd0e0b799f90490728e2e40abcc4")
 cur = connection.cursor()
-cur.execute('SELECT * FROM table_name')
+cur.execute('SELECT * FROM table_name:')
 for i in cur:
   print(i)
 cur.close()
