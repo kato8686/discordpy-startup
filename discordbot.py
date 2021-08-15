e820d7e872ac8d4eb921561c827fe2e63a9753a7
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-15 19:59:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,5 @@
 import psycopg2
 connection = psycopg2.connect("host=ec2-23-23-128-222.compute-1.amazonaws.com port=5432 dbname=dbmcuoiouunpmj user=qpjacaujnkxyil password=57acd55734d09130ac9abf498de632f05969bd0e0b799f90490728e2e40abcc4")
 cur = connection.cursor()
-cur.execute('select test')
-for i in cur:
-  print(i)
-cur.close()
+cur.execute('create table test (test int)')
 connection.close()
