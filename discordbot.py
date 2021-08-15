f11d294555ea6c16fbb65f5bf1970d78a0bf6406
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-15 18:55:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,6 @@
 import psycopg2
 connection = psycopg2.connect("host=ec2-23-23-128-222.compute-1.amazonaws.com port=5432 dbname=dbmcuoiouunpmj user=qpjacaujnkxyil password=57acd55734d09130ac9abf498de632f05969bd0e0b799f90490728e2e40abcc4")
 cur = connection.cursor()
-print(cur)
+cur.execute("select version()")
+for row in vur:
+  print(row)
