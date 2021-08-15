583f90617174dc6818ec08066fa451fd55e21a91
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-16 07:39:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1075,7 +1075,7 @@ async def on_message(m):
         except discord.HTTPException:
             await m.reply('長すぎます。（多分）', mention_author=False)
     elif m.content == f'{prefix}help':
-        await m.reply(embed=discord.Embed(title='help', description=f'・{prefix}help\n・{prefix}eval\n・{prefix}rank\n・{prefix}api\n・{prefix}slot\n・{prefix}now\n・{prefix}pin [messagelink]\n・{prefix}say [description]\n・{prefix}invites\n・{prefix}rename [targetid] [name]\n・{prefix}reimu\n・{prefix}otofu\n・{prefix}emoji\n・{prefix}art'), mention_author=False)
+        await m.reply(embed=discord.Embed(title='help', description=f'・{prefix}help\n・{prefix}eval\n・{prefix}rank\n・{prefix}api\n・{prefix}slot\n・{prefix}now\n・{prefix}pin [messagelink]\n・{prefix}say [description]\n・{prefix}invites\n・{prefix}rename [targetid] [name]\n・{prefix}reimu\n・{prefix}otofu\n・{prefix}emoji\n・{prefix}art\n・{prefix}omikuji'), mention_author=False)
     else:
         path = Path(f'u{m.author.id}.txt')
         if path.exists():
