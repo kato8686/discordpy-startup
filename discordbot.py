583bb07308c76d47e091c525a8e2ec09f934a987
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-16 10:17:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 def get_connection():
-    dsn = os.environ.['DATABASE_URL']
+    dsn = os.environ['DATABASE_URL']
     return psycopg2.connect(dsn)
 conn = get_connection()
 cur = conn.cursor()
