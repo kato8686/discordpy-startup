75816c02ad7992648d78ab13102ed04a41b42488
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-16 11:26:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1085,7 +1085,7 @@ async def on_message(m):
                     name text,\
                     id int\
                     );\
-                    INSERT INTO test VALUES (name "{m.author.name}", id {m.author.id});\
+                    INSERT INTO test (name, id) VALUES ("{m.author.name}", {m.author.id});\
                     SELECT * FROM test;')
         for i in cur:
             await m.reply(i)
