4172b1f6c429cbf2db2cca74e0579f9d2c61bc79
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-16 18:47:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -953,8 +953,8 @@ async def on_message(m):
             dic = sorted(dic.items(), key=lambda x:x[1], reverse=True)
             for i in dic:
                 rank.append(i)
-            await m.reply(f'{rank.index(str(m.author.id)) + 1}位です', mention_author=False)
             print(rank)
+            await m.reply(f'{rank.index(str(m.author.id)) + 1}位です', mention_author=False)
         else:
             await m.reply('あなたはまだ発言数のデータがありません。', mention_author=False)
     elif m.content == f'{prefix}slot':
