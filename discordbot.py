9d1bfc39343ff8936f8ab9d97071dd70cd499bbe
yuina8686 <76641185+yuina8686@users.noreply.github.com>
2021-08-16 12:58:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1082,11 +1082,6 @@ async def on_message(m):
             await m.reply('長すぎます。（多分）', mention_author=False)
     elif m.content == f'{prefix}help':
         await m.reply(embed=discord.Embed(title='help', description=f'・{prefix}help\n・{prefix}eval\n・{prefix}rank\n・{prefix}api\n・{prefix}slot\n・{prefix}now\n・{prefix}pin [messagelink]\n・{prefix}say [description]\n・{prefix}invites\n・{prefix}rename [targetid] [name]\n・{prefix}reimu\n・{prefix}otofu\n・{prefix}emoji\n・{prefix}art\n・{prefix}omikuji'), mention_author=False)
-    else:
-        cur.execute(f'CREATE TABLE IF NOT EXISTS userdeta (\
-                    id text,\
-                    count text\
-                    );')
 async def on_member_join(member):
     if member.guild.id == server_id:
         await member.send(embed=discord.Embed(title='公式鯖にようこそ！', description=welcome))
