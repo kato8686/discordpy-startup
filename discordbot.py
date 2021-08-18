2ec0aa966d4fc2fd590a40899a5dccfe8b5208b2
kato8686 <89115304+kato8686@users.noreply.github.com>
2021-08-18 14:36:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,6 +14,6 @@ guild_ids = [796546441702932481] # Put your server ID in this array.
 @slash.slash(name="test",
             description='どうでも良いテストコマンド')
 async def test(ctx): # Defines a new "context" (ctx) command called "ping."
-    await ctx.send('Hello World!!')
+    await ctx.send(ctx.guild.member_count)
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
