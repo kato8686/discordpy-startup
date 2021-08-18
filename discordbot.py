bb06f4864b4c2e793ddd858aa0f591ae2a8c5e58
kato8686 <89115304+kato8686@users.noreply.github.com>
2021-08-18 14:27:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,5 +8,11 @@ slash = SlashCommand(client, sync_commands=True) # Declares slash commands throu
 @client.event
 async def on_ready():
     print("Ready!")
+    
+guild_ids = [796546441702932481] # Put your server ID in this array.
+
+@slash.slash(name="ping", guild_ids=guild_ids)
+async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
+    await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
