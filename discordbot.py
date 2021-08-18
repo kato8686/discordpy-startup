f2d218752e4ef8ded4dc9130e7b3c3b34b5c5780
kato8686 <89115304+kato8686@users.noreply.github.com>
2021-08-18 15:41:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,8 @@ import discord
 from discord_slash import SlashCommand # Importing the newly installed library.
 import os
 from discord_slash.utils.manage_commands import create_option, create_choice, create_permission
-from discord_slash.model import SlashCommandPermissionType
+from discord_slash.model import SlashCommandPermissionType, ContextMenuType
+from discord_slash.context import MenuContext
 
 client = discord.Client(intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
@@ -13,32 +14,13 @@ async def on_ready():
     
 guild_ids = [796546441702932481] # Put your server ID in this array.
 
-@slash.slash(name="test",
-             description='どうでも良いテストコマンド',
-             options=[
-                 create_option(
-                     name='option',
-                     description='testing now',
-                     option_type=3,
-                     required=False,
-                     choices=[
-                         create_choice(
-                             name='yes',
-                             value='true'
-                         ),
-                         create_choice(
-                             name='no',
-                             value='false'
-                         )
-                     ]
-                 )
-             ],
-             permissions={
-                 796546441702932481: [
-                     create_permission(802152878855684106, SlashCommandPermissionType.USER, False)
-                 ]
-             })
-async def test(ctx, option: str): # Defines a new "context" (ctx) command called "ping."
-    await ctx.send(content=option)
+@slash.context_menu(target=ContextMenuType.MESSAGE,
+                    name="commandname",
+                    guild_ids=[796546441702932481])
+async def commandname(ctx: MenuContext):
+    await ctx.send(
+        content=f"Responded! The content of the message targeted: {ctx.target_message.content}",
+        hidden=True
+    )
 
 client.run(os.environ['DISCORD_BOT_TOKEN'])
