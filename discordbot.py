3ed8190ac34bb2e3e3473f34ecd9422296551ac0
kato8686 <89115304+kato8686@users.noreply.github.com>
2021-08-18 14:20:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,15 +1,11 @@
 import discord
-from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-import os
-from discord_slash.utils.manage_commands import create_option
+from discord_slash import SlashCommand # Importing the newly installed library.
 
-bot = commands.Bot(command_prefix='@', intents=discord.Intents.all())
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
 
-slash_client = SlashCommand(bot)
+@client.event
+async def on_ready():
+    print("Ready!")
 
-@slash_client.slash(name="test", description="This is just a test command, nothing more.", options=[create_option(name="optone", description="This is the first option we have.", option_type=3, required=False)])
-async def test(ctx, optone: str):
-    await ctx.send(content=f"I got you, you said {optone}!")
-
-bot.run(os.environ['DISCORD_BOT_TOKEN'])
+client.run(os.environ['DISCORD_BOT_TOKEN'])
