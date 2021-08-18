45ffe9a3ca7d56f6fcd9607b0c34daa2a8732d61
kato8686 <89115304+kato8686@users.noreply.github.com>
2021-08-18 13:58:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,4 @@
+"""
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
@@ -12,3 +13,47 @@ async def _slash_hello(ctx: SlashContext):
     await ctx.send('hello')
 
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
+"""
+import requests
+
+url = "https://discord.com/api/v8/applications/<APPLICATION ID>/guilds/<GUILD ID>/commands"
+
+json = {
+    "name": "blep",
+    "description": "Send a random adorable animal photo",
+    "options": [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": True,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": False
+        }
+    ]
+}
+
+headers = {
+    "Authorization": "Bot <BOT TOKEN>"
+}
+
+r = requests.post(url, headers=headers, json=json)
+print(r.status_code)
